729b7311c3b6155ddb43f300edabb53e83ce8559
Corrado Alfano <corrado.alfano10@gmail.com>
2020-03-06 11:23:04 -0800

    cleaned and organized index.php

--- index.php
+++ index.php
@@ -26,17 +26,19 @@
                 $subject = 'Contact Request From ' .$name;
                 $body = '<h2>Contact Request</h2>
                          <h4>Name</h4><p>'.$name.'</p>
-                         <h4>Name</h4><p>'.$email.'</p>
-                         <h4>Name</h4><p>'.$message.'</p>
+                         <h4>Email</h4><p>'.$email.'</p>
+                         <h4>Message</h4><p>'.$message.'</p>
                         ';
                 //
                 // Email Headers
                 $headers = "MIME-Version: 1.0" ."\r\n";
-                // .= means appending onto the existing variable, it will not replace variable value, it will add to it!
+                // .= means appending onto the existing variable, it will not replace variable value,
+                // it will add to it!
                 $headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
                 //
                 // Additional Headers
-                // // .= means appending onto the existing variable, it will not replace variable value, it will add to it!
+                // // .= means appending onto the existing variable, it will not replace variable value,
+                // it will add to it!
                 $headers .= "From: " .$name. "<".$email.">". "\r\n";
 
                 if(mail($toEmail, $subject, $body, $headers)) {
@@ -64,7 +66,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrinktofit=no">
         <title>Contact Form</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+              crossorigin="anonymous">
     </head>
     <body>
         <!-- Navbar created using bootstrap  -->
@@ -89,19 +93,29 @@
                  <div class="form-group">
                      <!-- User Name -->
                     <label>Full Name</label>
-                    <input type="text" name="name" class="form-control" value="<?php echo isset($name) ? $name : ''; ?>">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your name with anyone else.</small>
+                    <input type="text" name="name" class="form-control" value="
+                    <?php echo isset($name) ? $name : ''; ?>">
+                    <!-- Safe Warning for the User -->
+                    <small id="emailHelp" class="form-text text-muted">
+                        We'll never share your name with anyone else.
+                    </small>
                 </div>
                 <div class="form-group">
                     <!-- User Email Address -->
                     <label>Email Address</label>
-                    <input type="text" name="email" class="form-control" value="<?php echo isset($email) ? $email : ''; ?>">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <input type="text" name="email" class="form-control" value="
+                    <?php echo isset($email) ? $email : ''; ?>">
+                    <!-- Safe Warning for the User -->
+                    <small id="emailHelp" class="form-text text-muted">
+                        We'll never share your email with anyone else.
+                    </small>
                 </div>
                 <div class="form-group">
                     <!-- User Message -->
                     <label>Message</label>
-                    <textarea name="message" class="form-control"><?php echo isset($message) ? $message : ''; ?></textarea>
+                    <textarea name="message" class="form-control">
+                        <?php echo isset($message) ? $message : ''; ?>
+                    </textarea>
                 </div>
                 <br>
                 <button type="submit" name ="submit" class="btn btn-primary">Submit</button>
@@ -109,6 +123,10 @@
             <!-- End Contact Form -->
         </div>
         <!-- Footer -->
-        <footer class="navbar navbar-expand-lg navbar-dark bg-primary" style='color: #FFFFFF; position:fixed; bottom:0; width:100%; height:50px; display:inline-block; text-align:center;'> Corrado Alfano - Software Developer </footer>
+        <footer
+            class="navbar navbar-expand-lg navbar-dark bg-primary"
+            style='color: #FFFFFF; position:fixed; bottom:0; width:100%; height:50px; display:inline-block; text-align:center;'>
+            Corrado Alfano - Software Developer
+        </footer>
     </body>
 </html>
